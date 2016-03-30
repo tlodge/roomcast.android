b4ca83b3a33bea4dc617faffd73e737b1596bf85
Tom Lodge <tomlodge@tlodge.local>
2016-03-30 10:05:42 +0100

    first commit

---
+++ app/src/main/java/block49/com/buttonkit/MainActivity.java
@@ -0,0 +1,80 @@
+package block49.com.buttonkit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.PermissionRequest;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView myWebView;
+    private String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        myWebView.clearCache(true);
+        myWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+
+            }
+
+        });
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://wharfside.buttonkit.com");
+    }
+
+    public class WebC extends WebViewClient {
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+            super.onReceivedError(view,errorCode,description,failingUrl);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
